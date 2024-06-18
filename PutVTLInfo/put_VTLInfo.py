bae06bae926c69ffb4b601d385420cce98f838b9
LittleBear-6w6 <57203646+LittleBear-6w6@users.noreply.github.com>
2024-06-19 00:40:02 +0900

    Revert "文字列の拾い方を変更"
    This reverts commit 8ebc9af3ca9939b53696eaba1cdd4740a3540a26.

--- PutVTLInfo/put_VTLInfo.py
+++ PutVTLInfo/put_VTLInfo.py
@@ -1,30 +1,25 @@
 infile = './input.txt'
-outfile='./output.csv'
+outfile='./output.txt'
 datasetFlag = 0
- 
-dataName=''
-datalimit=''
- 
+
 outFile = open(outfile,'w',encoding='utf-8')
- 
+
 with open(infile) as file_object:
     for line in file_object:
         if 'GDG BASE' in line:
-            dataName=''
-            datalimit=''
             target='------'
             idx = line.find(target)
             dataName = line[idx + len(target):]
             datasetFlag=1
+        
         if datasetFlag == 1:
-            if 'LIMIT' in line:
-                targetst='LIMIT'
+            if 'LIMIT-----------------' in line:
+                targetst='-----------------'
                 idxst = line.find(targetst)
                 targetend='SCRATCH'
                 idxend = line.find(targetend)
                 datalimit = line[idxst + len(targetst):idxend]
-                datalimit = datalimit.replace('-','')
                 datasetFlag=0
- 
+
                 outFile.write(f'{dataName.strip()},{datalimit.strip()}\n')
 outFile.close()
